2ef7a6e6f150d7a34ea337afb5ce9940f42b990e
franblas <francois_blas@hotmail.com>
2015-06-10 14:44:43 +0200

    flickr

--- src/flickr.py
+++ src/flickr.py
@@ -37,6 +37,30 @@ class Flickr(API):
         res['url'].append(self._tools.key_test('_content',d['urls']['url'][0]))
         res['views'].append(self._tools.key_test('views',d))
         return res     
+
+    def _parsing_data3(self,data):
+        res = {'tags':list()}
+        for d in data['clusters']['cluster']:
+            taglist = [x for x in d['tag']]
+            for t in taglist:
+                res['tags'].append(self._tools.key_test('_content',t))
+        return res
+    
+    def _parsing_data4(self,data):
+        res = {'tags':list()}
+        for d in data['hottags']['tag']:
+            res['tags'].append(self._tools.key_test('_content',d))
+        return res    
+
+    def _parsing_data5(self,data):
+        res = {'categories':list(),'author':list(),'url':list(),'title':list(),'date':list()}
+        for d in data['responseData']['feed']['entries']:
+            res['categories'].append(self._tools.key_test('categories',d,'list'))
+            res['author'].append(self._tools.key_test('author',d))
+            res['url'].append(self._tools.key_test('link',d))
+            res['title'].append(self._tools.key_test('title',d))
+            res['date'].append(self._tools.key_test('publishedDate',d))
+        return res 
     
     def search(self,text='',limit=10):
         text = text.replace(' ','+')
@@ -50,3 +74,29 @@ class Flickr(API):
         data = self._tools.data_from_url_flickr(url)
         self._increment_nb_call()
         return self._parsing_data2(data)      
+
+    def get_tags(self,text=''):
+        text = text.replace(' ','+')
+        url = self._api_url+'rest/?method=flickr.tags.getClusters&format=json&api_key='+self._api_key+'&tag='+text
+        data = self._tools.data_from_url_flickr(url)
+        self._increment_nb_call()
+        return self._parsing_data3(data)
+
+    def get_cluster_photos(self,text=''):
+        text = text.replace(' ','+')
+        url = self._api_url+'rest/?method=flickr.tags.getClusterPhotos&format=json&api_key='+self._api_key+'&cluster_id='+text
+        data = self._tools.data_from_url_flickr(url)
+        self._increment_nb_call()
+        return self._parsing_data(data) 
+
+    def get_hot_tags(self,limit=10):
+        url = self._api_url+'rest/?method=flickr.tags.getHotList&format=json&api_key='+self._api_key+'&count='+str(limit)
+        data = self._tools.data_from_url_flickr(url)
+        self._increment_nb_call()
+        return self._parsing_data4(data)        
+    
+    def get_public_feed(self,limit=10):
+        url = self._api_url+'feeds/photos_public.gne&output=json&num='+str(limit)
+        data = self._tools.rss_parser(url)
+        self._increment_nb_call()
+        return self._parsing_data5(data)
